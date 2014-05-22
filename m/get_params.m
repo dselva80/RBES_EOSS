3c90d6d3b32fbcc1a3d65796c1c25a524bed6031
dselva80 <daniel.selva@gmail.com>
2014-05-22 17:25:00 -0400

    solved problem of fast and slow

--- m/get_params.m
+++ m/get_params.m
@@ -1,7 +1,7 @@
 function params = get_params()
     persistent parameters
     if isempty(parameters)
-        folder = 'C:\Users\DS925\Documents\GitHub\Selva research group\RBES Demo May2014';
+        folder = 'C:\Users\DS925\Documents\GitHub\RBES_EOSS';
         parameters = rbsa.eoss.local.Params(folder,'CRISP-ATTRIBUTES','test','normal','');
     end
     params = parameters;
